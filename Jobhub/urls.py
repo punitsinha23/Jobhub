c27978498e6a64b462ec6c8f4c176ae1eb086841
punit <punitsinha495@gmail.com>
2025-08-31 12:19:16 +0530

    added

--- Jobhub/urls.py
+++ Jobhub/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('JobHub_app.urls')),
 ]
+
+if settings.DEBUG:  # only in dev
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
